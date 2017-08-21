dc02727bbed481877fd27bb719784354ab63bb5a
Dave Man <dev.the.ripper@gmail.com>
2017-08-21 23:38:18 +0400

    Eod and cleanup

---
+++ ui/compiled/40e8ff81991f4ddc74145c0fa693cfe11681c0e5_0.file.header.tpl.php
@@ -0,0 +1,32 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-08-21 23:35:30
+  from "C:\Bitnami\wampstack-5.6.31-0\apache2\htdocs\iBill\iBill\ui\theme\ibilling\sections\header.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_599b3602b9f136_08061531',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '40e8ff81991f4ddc74145c0fa693cfe11681c0e5' => 
+    array (
+      0 => 'C:\\Bitnami\\wampstack-5.6.31-0\\apache2\\htdocs\\iBill\\iBill\\ui\\theme\\ibilling\\sections\\header.tpl',
+      1 => 1479216128,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_599b3602b9f136_08061531 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender(((string)$_smarty_tpl->tpl_vars['tplheader']->value).".tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, true);
+?>
+
+<?php if ($_smarty_tpl->tpl_vars['content_inner']->value != '') {?>
+    <?php echo $_smarty_tpl->tpl_vars['content_inner']->value;?>
+
+<?php }
+}
+}
---
+++ ui/compiled/a0919e2701b96c2434388f0d02c3f319150a4b18_0.file.footer.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-08-21 23:35:30
+  from "C:\Bitnami\wampstack-5.6.31-0\apache2\htdocs\iBill\iBill\ui\theme\ibilling\sections\footer.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_599b3602da6a27_00125715',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'a0919e2701b96c2434388f0d02c3f319150a4b18' => 
+    array (
+      0 => 'C:\\Bitnami\\wampstack-5.6.31-0\\apache2\\htdocs\\iBill\\iBill\\ui\\theme\\ibilling\\sections\\footer.tpl',
+      1 => 1479216128,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_599b3602da6a27_00125715 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender(((string)$_smarty_tpl->tpl_vars['tplfooter']->value).".tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, true);
+}
+}
