4ec150e1a605eac62cacaa41fd7fb666aef76e57
Dave Man <dev.the.ripper@gmail.com>
2017-08-21 23:34:37 +0400

    Initiate...2

--- application/config.php
+++ application/config.php
@@ -3,5 +3,5 @@ $db_host	    = 'localhost';
 $db_user        = 'root';
 $db_password	= 'bionicman';
 $db_name	    = 'ibill';
-define('APP_URL', 'http://sand-box/ibill');
+define('APP_URL', 'http://sand-box/ibill/iBill');
 $_app_stage = 'Dev'; // You can set this variable Live to Dev to enable ibilling Debug
